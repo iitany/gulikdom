9df73c31377a6cbb476f7e3de474a52123aca638
iitany <138595835+iitany@users.noreply.github.com>
2023-07-28 15:16:41 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '7954b5d9-02a8-4c79-87f2-52afd5305281';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
